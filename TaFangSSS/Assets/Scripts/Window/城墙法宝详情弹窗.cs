3c87496c940019e3f952c118b19585ecaf9c7a9c
wangweikang3 <wangweikang3@tal.com>
2026-08-04 10:43:07 +0800

    完善2

--- TaFangSSS/Assets/Scripts/Window/城墙法宝详情弹窗.cs
+++ TaFangSSS/Assets/Scripts/Window/城墙法宝详情弹窗.cs
@@ -9,15 +9,19 @@ using UnityEngine.UI;
 
 public class 城墙法宝详情弹窗 : MonoBehaviour
 {
+    public RectTransform bgTrans;
     public Button maskButton;
     public Button ExitButton;
     public Image 艺术字;
     public Image image;
     public TextMeshProUGUI name;
     public TextMeshProUGUI desc;
-    public TextMeshProUGUI 效果;
+    public TextMeshProUGUI 效果info;
+    public TextMeshProUGUI 效果无;
+    public TextMeshProUGUI 效果tip;
     public TextMeshProUGUI 升级奖励;
     public GameObject 升级信息Content;
+    public GameObject 升级信息;
     [NonSerialized] public 城墙道具Type 城墙道具Type;
 
     public void Show()
@@ -28,33 +32,50 @@ public class 城墙法宝详情弹窗 : MonoBehaviour
         image.sprite = ResourcesConfig.Get城墙Sprite(城墙道具Type);
         name.text = 城墙Config.城墙道具名Dic[城墙道具Type];
         desc.text=城墙Config.城墙道具介绍Dic[城墙道具Type];
-        效果.text = 城墙Config.城墙道具属性Info[城墙Config.城墙道具属性Dic[城墙道具Type]];
-        foreach (Transform item in 升级信息Content.transform)
+        if (quality >= QualityType.宙品)
         {
-            Destroy(item.gameObject);
-        }
+            效果info.text = 城墙Config.城墙道具属性Info[城墙Config.城墙道具属性Dic[城墙道具Type]];
+            效果tip.gameObject.SetActive(true);
+            效果无.gameObject.SetActive(false);
+            升级信息.gameObject.SetActive(true);
+            foreach (Transform item in 升级信息Content.transform)
+            {
+                Destroy(item.gameObject);
+            }
 
-        List<string> list = null;
+            List<string> list = null;
 
-        if (城墙Config.城墙道具QualityDic[城墙道具Type] >= QualityType.宙品)
-        {
-            list = 城墙Config.城墙道具属性升级Info[城墙Config.城墙道具属性Dic[城墙道具Type]];
-        }else if (城墙Config.城墙道具QualityDic[城墙道具Type] == QualityType.宇品)
-        {
-            list = 城墙Config.橙色城墙道具升级Info;
+            if (城墙Config.城墙道具QualityDic[城墙道具Type] >= QualityType.宙品)
+            {
+                list = 城墙Config.城墙道具属性升级Info[城墙Config.城墙道具属性Dic[城墙道具Type]];
+            }else if (城墙Config.城墙道具QualityDic[城墙道具Type] == QualityType.宇品)
+            {
+                list = 城墙Config.橙色城墙道具升级Info;
+            }
+            else
+            {
+                list = 城墙Config.紫色城墙道具升级Info;
+            }
+        
+            for (int i = 0; i <= 4; i++)
+            {
+                var item = Instantiate(Resources.Load("Prefabs/Window/城墙道具升级信息item"), 升级信息Content.transform)
+                    .GetComponent<城墙道具升级信息item>();
+                item.城墙道具Type = 城墙道具Type;
+                item.解锁level = 城墙Config.城墙道具升级List[i];
+                item.SetItem();
+            }
+            bgTrans.localPosition = new Vector3(0, 0, 0);
+            bgTrans.sizeDelta = new Vector2(523, 840);
         }
         else
         {
-            list = 城墙Config.紫色城墙道具升级Info;
-        }
-        
-        for (int i = 0; i <= 4; i++)
-        {
-            var item = Instantiate(Resources.Load("Prefabs/Window/城墙道具升级信息item"), 升级信息Content.transform)
-                .GetComponent<城墙道具升级信息item>();
-            item.城墙道具Type = 城墙道具Type;
-            item.解锁level = 城墙Config.城墙道具升级List[i];
-            item.SetItem();
+            效果info.gameObject.SetActive(false);
+            效果tip.gameObject.SetActive(false);
+            效果无.gameObject.SetActive(true);
+            升级信息.gameObject.SetActive(false);
+            bgTrans.localPosition = new Vector3(0, 0, 0);
+            bgTrans.sizeDelta = new Vector2(523, 420);
         }
     }
 
